f1ea882cf67865e9ded481b3bb07aefe0f5f050c
avran02 <avran2002@gmail.com>
2023-07-27 13:02:13 +0300

    added genre choosing

--- enter.py
+++ enter.py
@@ -1,20 +1,31 @@
 from main import Parser
 
+def continue_(genre, parser):
+	parser.get_pages_list(genre)
+	try:
+		songs_num = int(input('-Введите количество песен для скачивания(не больше 50):\n'))
+		page_num = int(input(f'-С какой страницы? \n-Всего доступно {parser.num_pages} страниц\n-Для загрузки со случайной страницы введите 0\n'))
+	except:
+		print('-Вы ввели что-то не то, перезапустите скрипт')
+		exit()
+	if page_num > parser.num_pages:
+		print('-Столько страниц там нет, перезапустите скрипт')
+		exit()
+	elif page_num == 0:
+		parser.parce_random(songs_num)
+	else:
+		parser.parce_any(songs_num, page_num)
+
 
 if __name__ == '__main__':
-
-	executor = Parser()
-	try:
-		songs_num = int(input('\t-Введите количество песен для скачивания(не больше 50):\n'))
-		page_num = int(input(f'\t-С какой страницы? \n\t-Всего доступно {executor.num_pages} страниц\n\t-Для загрузки со случайной страницы введите 0\n'))
-	except:
-		print('\t-Вы ввели что-то не то, перезапустите скрипт')
-		exit
-	if page_num > executor.num_pages:
-		print('\t-Столько страниц там нет, перезапустите скрипт')
-		exit
-	elif page_num == 0:
-		executor.parce_random(songs_num)
+	parser = Parser()
+	genres = parser.genres
+	for genre in genres:
+		print(f'@{genre}')
+	g = input('-Введите жанр, который хотите скачать\n-Ввести нужно символ в символ, без собаки!\n')
+	if g in genres:
+		continue_(genres[g], parser)
 	else:
-		executor.parce_any(songs_num, page_num)
+		print('-Такого жанра нет в списке, перезапустите скрипт')
+
 
--- main.py
+++ main.py
@@ -5,33 +5,51 @@ import random
 import aiofiles
 import requests
 
+
 class Parser:
     HEADERS = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
     }
-    root_url='https://rus.mp3xa.me/eng_rock/'
-    page = None
-    num_songs = None
-    num_pages = None
-    download_pages_links = None
+    ROOT_URL='https://rus.mp3xa.me/'
+    
 
     def __init__(self):
-        print('\t-Получаю данные со страницы...')
-        self.page = self._get_page_html(self.root_url)
+        self.page = None
+        self.num_songs = None
+        self.num_pages = None
+        self.download_pages_links = None
+        self.genres = None
+        self.genre = None
+        print('-Ищу, в каких жанрах есть музыка...')
+        self._parce_genres()
+
+    def get_pages_list(self, genre):
+        self.genre = genre[1:]
+        self.page = self._get_page_html(f'{self.ROOT_URL}{self.genre}')
         self.num_pages = self._get_num_pages(self.page)
 
     def parce_random(self, num_songs):
     	self.num_songs = num_songs
-    	print('\t-Получаю данные с другой страницы..)')
-    	random_page = self._get_page_html(f'{self.root_url}page/{random.randint(1, num_songs)}')
+    	print('-Ищу музыку по этому жанру..)')
+    	random_page = self._get_page_html(f'{self.ROOT_URL}{self.genre}page/{random.randint(1, num_songs)}')
     	self._parce(random_page)
-    	
 
     def parce_any(self, num_songs, page):
     	self.num_songs = num_songs
-    	print('\t-Получаю данные с другой страницы..)')
-    	page_doc = self._get_page_html(f'{self.root_url}page/{page}') # todo: Переименовать переменные
+    	print('-Ищу музыку по этому жанру..)')
+    	page_doc = self._get_page_html(f'{self.ROOT_URL}page/{page}') # todo: Переименовать переменные
     	self._parce(page_doc)
+
+    def _parce_genres(self):
+        page = self._get_page_html(f'{self.ROOT_URL}genres.html')
+        section = page.find('div', class_='content card song_year').find('div', class_='cat_wrap')
+        genre_cards = section.find_all('div', class_='cat_item big')
+        genres = dict()
+        for card in genre_cards:
+            link = card.find('a')['href']
+            name = card.find('span').text
+            genres[name] = link
+        self.genres = genres
 
     def _parce(self, url):
     	songs = url.find('div', class_='songs-wrap').find_all('div', class_='plyr-item')
@@ -44,9 +62,7 @@ class Parser:
     	    name = song_wrap.find('div', class_='song_name').text.strip()
     	    link = song_wrap.find('a', class_='download_btn')['href']
     	    self.download_pages_links.append({'name': name, 'link': link})
-
     	asyncio.run(self._gather_files())
-
 
     async def _get_download_link(self, session, url):
         async with session.get(url) as response:
@@ -56,25 +72,25 @@ class Parser:
                 download_link = soup.find('a', class_='download_btn')['href']
                 return download_link
             else:
-                print(f"\t-Не удалось получить ссылку на загрузку с URL: {url}, Ответ сервера: {response.status}")
+                print(f"-Не удалось получить ссылку на загрузку с URL: {url}, Ответ сервера: {response.status}")
                 return None
 
     async def _download_file(self, session, url, filename):
-        print(f"\t-Начинаю загрузку файла '{filename}'")
+        print(f"-Начинаю загрузку файла '{filename}'")
         async with session.get(url) as response:
             if response.status == 200:
-                async with aiofiles.open(filename, 'wb') as file:
+                async with aiofiles.open(f'downloads/{filename}', 'wb') as file:
                     while True:
                         chunk = await response.content.read(1024)
                         if not chunk:
                             break
                         await file.write(chunk)
-                print(f"\t-Файл '{filename}' успешно скачан.")
+                print(f"-Файл '{filename}' успешно скачан.")
             else:
-                print(f"\t-Не удалось скачать файл с URL: {url}")
+                print(f"-Не удалось скачать файл с URL: {url}")
 
     async def _gather_files(self):
-        print('\t-Собираю url адреса для загрузки')
+        print('-Собираю url адреса для загрузки')
         async with aiohttp.ClientSession(headers=self.HEADERS) as session:
             tasks = []
             max_concurrent_requests = 5
@@ -98,29 +114,10 @@ class Parser:
             page = bs(response.text, 'lxml')
             return page
         else:
-            print(f'\t-Сервер вернул ответ {response.status_code}')
+            print(f'-Сервер вернул ответ {response.status_code}')
             return
 
     def _get_num_pages(self, doc):
         pages = doc.find('section', class_='popular full').find('div', class_='paginator').find_all('li')
         num_pages = int(pages[-2].find('a').text)
         return num_pages
-
-# if __name__ == '__main__':
-#     parser = Parser()
-#     page = parser.get_page_html('https://rus.mp3xa.me/eng_rock/')
-#     num_pages = parser.get_num_pages(page)
-
-#     random_page = parser.get_page_html(f'https://rus.mp3xa.me/eng_rock/page/{random.randint(1, num_pages)}')
-#     songs = random_page.find('div', class_='songs-wrap').find_all('div', class_='plyr-item')
-#     download_pages_links = list()
-#     count = 0
-#     for song_wrap in songs:
-#         if count == 3:
-#             break
-#         count += 1
-#         name = song_wrap.find('div', class_='song_name').text.strip()
-#         link = song_wrap.find('a', class_='download_btn')['href']
-#         download_pages_links.append({'name': name, 'link': link})
-
-#     asyncio.run(parser._gather_files(download_pages_links))
